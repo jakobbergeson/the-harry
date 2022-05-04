dd6829656d6afa123d4dec4192cb0ccfa9946f8a
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2022-05-04 13:19:12 -0700

    Add reusable Local Title component.

---
+++ components/LocalTitle.js
@@ -0,0 +1,13 @@
+import styles from '@/styles/LocalTitle.module.css';
+
+export default function LocalTitle({ children }) {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.mainTitle}>
+          <h1>{children}</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
