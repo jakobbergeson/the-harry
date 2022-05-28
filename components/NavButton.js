1641553a2512b1b74bbe777f034eb79878f6daf7
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2022-05-27 17:05:22 -0700

    Add new assets for for button background images.

--- components/NavButton.js
+++ components/NavButton.js
@@ -7,14 +7,5 @@ export default function NavButton({ bg, to, children }) {
     <Link href={to}>
       <a className={bg ? styles.containerBg : styles.container}>{children}</a>
     </Link>
-    // <Link
-    //   href={to}
-    //   className={bg ? styles.containerBg : styles.container}
-    // >
-    //   <a
-    //   >
-    //     {children}
-    //   </a>
-    // </Link>
   );
 };
