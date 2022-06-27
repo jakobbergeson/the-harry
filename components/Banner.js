3f1734adc714ea7ea38c406ce81c94d208fecda1
Jakob Bergeson <jakobbergeson.dev@gmail.com>
2022-06-27 16:39:32 -0700

    hotfix-3.

--- components/Banner.js
+++ components/Banner.js
@@ -4,9 +4,9 @@ import style from '../styles/Banner.module.css';
 export default function Banner() {
   return (
     <Link
-      href='/' className={style.promoBar}
+      href='/'
     >
-      <a>
+      <a className={style.promoBar}>
         <ul>
           <li>Join our club! Receive 25% off your first online order</li>
         </ul>
